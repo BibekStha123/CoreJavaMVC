683c62371f5fe39a850978d83827fde0b4f01bd0
BibekStha123 <34579460+BibekStha123@users.noreply.github.com>
2018-06-21 21:28:52 +0545

    Add files via upload

---
+++ student.java
@@ -0,0 +1,30 @@
+package Model;
+
+public class student {
+
+	private String name;
+	private String roll;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getRoll() {
+		return roll;
+	}
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+	
+	
+	
+}

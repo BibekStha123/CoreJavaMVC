22fef9d15ea20756f2a3d89105f9c3fa22b93a51
BibekStha123 <34579460+BibekStha123@users.noreply.github.com>
2018-06-21 21:28:27 +0545

    Add files via upload

---
+++ studentview.java
@@ -0,0 +1,13 @@
+package View;
+
+public class studentview {
+
+	public void printDetails(String name, String roll, String add)
+	{
+		System.out.println("Student: ");
+		System.out.println("Name: "+name);
+		System.out.println("Roll no.: "+roll);
+		System.out.println("Address: "+add);
+	}
+	
+}
